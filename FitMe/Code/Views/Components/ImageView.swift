c037d5246f391a072b97c2cc4d1ca0c8c3ec13a3
Shenia Scherer <sh.scherer1808@gmail.com>
2025-01-02 21:50:49 +0100

    fix: update imageview

--- FitMe/Code/Views/Components/ImageView.swift
+++ FitMe/Code/Views/Components/ImageView.swift
@@ -4,16 +4,42 @@ struct ImageView: View {
     let imageURL: URL?
     let size: CGFloat
     let cornerRadius: CGFloat
+    let systemName: String
     
     static func getFullImageURL(_ path: String?) -> URL? {
         guard let path = path else { return nil }
         return URL(string: path)
     }
+
+    static func getSystemImageName(_ category: String?) -> String {
+        switch category {
+            case "Abs":
+                return "figure.core.training" 
+            case "Arms":
+                return "dumbbell.fill" 
+            case "Back":
+                return "figure.strengthtraining.functional" 
+            case "Calves":
+                return "figure.walk" 
+            case "Cardio":
+                return "heart.fill" 
+            case "Chest":
+                return "figure.strengthtraining.functional"
+            case "Legs":
+                return "figure.run" 
+            case "Shoulders":
+                return "figure.strengthtraining.functional"
+            default:
+                return "dumbbell.fill"
+        }
+    }
+
     
-    init(imageURL: URL?, size: CGFloat = 60, cornerRadius: CGFloat = 8) {
+    init(imageURL: URL?, size: CGFloat = 60, cornerRadius: CGFloat = 8, systemName: String = "dumbbell.fill") {
         self.imageURL = imageURL
         self.size = size
         self.cornerRadius = cornerRadius
+        self.systemName = systemName
     }
     
     var body: some View {
@@ -24,7 +50,7 @@ struct ImageView: View {
                 .frame(width: size, height: size)
                 .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
         } placeholder: {
-            Image(systemName: "dumbbell.fill")
+            Image(systemName: systemName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: size/2, height: size/2)
