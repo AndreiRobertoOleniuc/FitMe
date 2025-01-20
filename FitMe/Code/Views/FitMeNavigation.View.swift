af72c4e76bfb06a9e5eaccbbe9e5275aa4ce27d5
Shenia Scherer <sh.scherer1808@gmail.com>
2025-01-20 17:45:48 +0100

    fix: tab view content bleed through

--- FitMe/Code/Views/FitMeNavigation.View.swift
+++ FitMe/Code/Views/FitMeNavigation.View.swift
@@ -1,6 +1,5 @@
 import SwiftUI
 import SwiftData
-import Charts
 
 #Preview {
     ModelContainerPreview {
@@ -15,28 +14,38 @@ import Charts
 
 struct FitMeNavigation: View {
     var body: some View {
-        NavigationStack{
-            TabView {
-                RunningWorkoutView()
-                    .tabItem {
-                        Label("Running", systemImage: "play.fill")
-                    }
-                    .tag(1)
-                WorkoutView()
-                    .tabItem {
-                        Label("Workouts", systemImage: "dumbbell")
-                    }
-                    .tag(2)
-                PastWorkoutSession()
-                    .tabItem {
-                        Label("Past", systemImage: "clock")
-                    }
-                    .tag(3)
-                StatsView()
-                    .tabItem {
-                        Label("Stats", systemImage: "chart.bar.fill")
-                    }
-                    .tag(4)
+        NavigationStack {
+            ZStack {
+                VStack {
+                    Spacer()
+                    Rectangle()
+                        .fill(Color(uiColor: .systemBackground))
+                        .frame(height: UITabBarController().tabBar.frame.height)
+                        .ignoresSafeArea()
+                }
+                TabView {
+                    RunningWorkoutView()
+                        .tabItem {
+                            Label("Running", systemImage: "play.fill")
+                        }
+                        .tag(1)
+                    WorkoutView()
+                        .tabItem {
+                            Label("Workouts", systemImage: "dumbbell")
+                        }
+                        .tag(2)
+                    PastWorkoutSession()
+                        .tabItem {
+                            Label("Past", systemImage: "clock")
+                        }
+                        .tag(3)
+                    StatsView()
+                        .tabItem {
+                            Label("Stats", systemImage: "chart.bar.fill")
+                        }
+                        .tag(4)
+                }
+            
             }
         }
     }
