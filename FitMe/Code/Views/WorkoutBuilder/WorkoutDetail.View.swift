d75eea4f7ccb1bf2ad5967eaaeaca2f7361fe79a
Andrei Roberto Oleniuc <andreioleniucroberto@gmail.com>
2025-01-28 17:20:35 +0100

    ref: move detail row into own file

--- FitMe/Code/Views/WorkoutBuilder/WorkoutDetail.View.swift
+++ FitMe/Code/Views/WorkoutBuilder/WorkoutDetail.View.swift
@@ -52,20 +52,3 @@ struct WorkoutDetailView: View {
         }
     }   
 }
-
-
-struct DetailRow: View {
-    let title: String
-    let value: String
-    
-    var body: some View {
-        HStack {
-            Text(title)
-                .foregroundColor(.secondary)
-            Spacer()
-            Text(value)
-                .bold()
-        }
-        .padding(.horizontal)
-    }
-}
